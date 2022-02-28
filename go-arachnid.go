e1bbf948b5a49d4b254d3929049ac175d8f5af1e
m4jrt0m <m4jrt0m@gmail.com>
2022-02-28 19:55:46 +0000

    stuff

--- go-arachnid.go
+++ go-arachnid.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+
+	curl "github.com/andelf/go-curl"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	var url string
+	flag.StringVar(&url, "url", "", "The url to search for")
+	flag.Parse()
+
+	fmt.Println(url)
+
+	page := get_page(url)
+	fmt.Println(page)
+}
+
+func get_page(url string) string {
+	easy := curl.EasyInit()
+	defer easy.Cleanup()
+
+	easy.Setopt(curl.OPT_URL, url)
+
+	err := easy.Perform()
+	if err != nil {
+		fmt.Println("ERROR: ${err}\n")
+	}
+
+	return ""
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-arachnid
 
 go 1.17
+
+require github.com/andelf/go-curl v0.0.0-20200630032108-fd49ff24ed97 // indirect
